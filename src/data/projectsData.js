823b2c5b82a97846a0f818a45ccbeefc6fb64ce0
Tarek <83262890+tarek102@users.noreply.github.com>
2023-01-25 16:33:50 +0200

    Update projects data

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -20,26 +20,9 @@ export const projectsData = [
         demo: 'https://rails-pet-shop.onrender.com/',
         image: 'https://ik.imagekit.io/4t6ufpirwu/Capture.PNG?ik-sdk-version=javascript-1.4.3&updatedAt=1674039529879'
     },
+    
     {
         id: 2,
-        projectName: 'Hibrew',
-        projectDesc: 'An e-commerce store in the UAE that sells coffee makers.',
-        tags: ['WordPress','Elementor','Woocommerce'],
-        code: 'https://hibrew.ae/',
-        demo: 'https://hibrew.ae/',
-        image: 'https://ik.imagekit.io/4t6ufpirwu/Capture2.PNG?ik-sdk-version=javascript-1.4.3&updatedAt=1674039918505'
-    },
-    {
-        id: 3,
-        projectName: 'MK interior design',
-        projectDesc: 'MK Designs is one of Cairo\'s leading interior design firms. offering upscale interior decorating services',
-        tags: ['WordPress', 'Elementor pro'],
-        code: 'https://www.mk-interiordesigns.com/',
-        demo: 'https://www.mk-interiordesigns.com/',
-        image: 'https://ik.imagekit.io/4t6ufpirwu/mk-design.PNG?ik-sdk-version=javascript-1.4.3&updatedAt=1674123284585'
-    },
-    {
-        id: 4,
         projectName: 'Crypto currencies',
         projectDesc: 'A web application that displays information about popular cryptocurrencies and was built using the Binance API',
         tags: ['JavaScript', 'React', 'Redux'],
@@ -48,16 +31,7 @@ export const projectsData = [
         image: 'https://ik.imagekit.io/4t6ufpirwu/Capture3.PNG?ik-sdk-version=javascript-1.4.3&updatedAt=1674122687605'
     },
     {
-        id: 5,
-        projectName: 'PAM Dubai',
-        projectDesc: 'A Dubai-based marketing firm specialising in SEO and Facebook Ads.',
-        tags: ['WordPress', 'Elementor', 'Yoast'],
-        code: 'https://pamseodubai.com/',
-        demo: 'https://pamseodubai.com/',
-        image: 'https://ik.imagekit.io/4t6ufpirwu/pam_.PNG?ik-sdk-version=javascript-1.4.3&updatedAt=1674123122172'
-    },
-    {
-        id: 5,
+        id: 3,
         projectName: 'Movie web app',
         projectDesc: 'An online application that lists movies and provides information about each one',
         tags: ['Webpack', 'API', 'JavaScript', 'HTML', 'CSS'],
